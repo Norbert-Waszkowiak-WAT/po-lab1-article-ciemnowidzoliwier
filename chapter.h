15a8337a50b5b7fe7cd51fc1b736e8611bc8fcac
ciemnowidzoliwier <oliwiergabryjeluk@gmail.com>
2025-12-11 07:40:02 +0000

    adadadadad

--- chapter.h
+++ chapter.h
@@ -1,12 +1,25 @@
-#include<iostream>
+#ifndef CHAPTER_H
+#define CHAPTER_H
+ 
+#include <iostream>
+#include "author.h"
+#include "article.h"
 using namespace std;
-class Chapter{
-    private;
-    string title;
-    Author author;
-    int chapterNumber;
-    public:
-    Chapter();
-    Chapter(string chapterTitle, Author chapterAuthor, int number)
-Chapter(Chapter )
-}
+ 
+class Chapter {
+private:
+ string title;
+ Author author;
+ int chapterNumber;
+public:
+ Chapter();
+ Chapter(string chapterTitle, Author chapterAuthor, int number);
+ Chapter(Chapter& other);
+ Chapter(Article &article);
+ void displayInfo();
+ string getTitle();
+ Author getAuthor();
+ int getChapterNumber();
+};
+ 
+#endif
