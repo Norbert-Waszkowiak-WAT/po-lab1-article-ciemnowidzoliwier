26b676633ee4325d09fce0b31e47f83753109a7a
ciemnowidzoliwier <oliwiergabryjeluk@gmail.com>
2025-12-11 07:28:35 +0000

    artikel

---
+++ chapter.h
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class Chapter{
+    private;
+    string title;
+    Author author;
+    int chapterNumber;
+    public:
+    Chapter();
+    Chapter(string chapterTitle, Author chapterAuthor, int number)
+Chapter(Chapter )
+}
